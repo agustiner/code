80b00f8e043141e209e33fdd6ccc72cd5d22d6bb
Agustin Romero <89935000+aer7@users.noreply.github.com>
2022-12-14 08:30:58 -0800

    Add acts-odd-optuna

---
+++ acts-odd-optuna/simulate-odd.py
@@ -0,0 +1,105 @@
+import pathlib, acts, acts.examples
+import acts.examples.simulation
+
+from acts.examples.simulation import (
+    MomentumConfig,
+    EtaConfig,
+    ParticleConfig,
+    ParticleSelectorConfig,
+)
+from acts.examples.reconstruction import (
+    TruthSeedRanges,
+    CKFPerformanceConfig,
+    VertexFinder,
+    TrackSelectorRanges,
+)
+from acts.examples.odd import getOpenDataDetector
+import datetime
+import os
+
+def getActsSourcePath():
+    return pathlib.Path("/home/user1/code/acts/source")
+
+def getOpenDataDetectorPath():
+    return getActsSourcePath() / "thirdparty" / "OpenDataDetector"
+
+
+logger = acts.logging.getLogger("full_chain_odd")
+logger.info("Starting full_chain_odd")
+u = acts.UnitConstants
+oddPath = getOpenDataDetectorPath()
+outputDir = pathlib.Path.cwd()
+
+# acts.examples.dump_args_calls(locals())  # show python binding calls
+oddMaterialMap = oddPath / "data/odd-material-maps.root"
+oddDigiConfig = oddPath / "config/odd-digi-smearing-config.json"
+oddSeedingSel = oddPath / "config/odd-seeding-config.json"
+oddMaterialDeco = acts.IMaterialDecorator.fromFile(oddMaterialMap)
+
+detector, trackingGeometry, decorators = getOpenDataDetector(
+    oddPath, mdecorator=oddMaterialDeco
+)
+field = acts.ConstantBField(acts.Vector3(0.0, 0.0, 2.0 * u.T))
+rnd = acts.examples.RandomNumbers(seed=42)
+
+s = acts.examples.Sequencer(events=1, numThreads=-1, outputDir=str(outputDir))
+
+logger.info('Using pythia to make ttbar events.')
+
+acts.examples.simulation.addPythia8(
+    s,
+    hardProcess=["Top:qqbar2ttbar=on"],
+    npileup=10,
+    vtxGen=acts.examples.GaussianVertexGenerator(
+        stddev=acts.Vector4(0.0125 * u.mm, 0.0125 * u.mm, 55.5 * u.mm, 5.0 * u.ns),
+        mean=acts.Vector4(0, 0, 0, 0),
+    ),
+    rnd=rnd,
+    outputDirRoot=outputDir
+)
+
+acts.examples.simulation.addFatras(
+    s,
+    trackingGeometry,
+    field,
+    ParticleSelectorConfig(eta=(-3.0, 3.0), pt=(150 * u.MeV, None), removeNeutral=True),
+    outputDirRoot=outputDir,
+    rnd=rnd,
+)
+
+acts.examples.simulation.addDigitization(
+    s,
+    trackingGeometry,
+    field,
+    digiConfigFile=oddDigiConfig,
+    outputDirRoot=outputDir,
+    rnd=rnd,
+)
+
+acts.examples.reconstruction.addSeeding(
+    s,
+    trackingGeometry,
+    field,
+    TruthSeedRanges(pt=(1.0 * u.GeV, None), eta=(-3.0, 3.0), nHits=(9, None)),
+    geoSelectionConfigFile=oddSeedingSel,
+    outputDirRoot=outputDir
+)
+
+acts.examples.reconstruction.addCKFTracks(
+    s,
+    trackingGeometry,
+    field,
+    CKFPerformanceConfig(ptMin=1.0 * u.GeV, nMeasurementsMin=6),
+    outputDirRoot=outputDir
+)
+
+acts.examples.reconstruction.addVertexFitting(
+    s,
+    field,
+    TrackSelectorRanges(pt=(1.0 * u.GeV, None), absEta=(None, 3.0), removeNeutral=True),
+    vertexFinder=VertexFinder.Iterative,
+    trajectories="trajectories",
+    outputDirRoot=outputDir
+)
+
+s.run()
