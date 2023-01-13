2d32b8523a0e8b3671a553642e531376f17ebb8d
Agustin Romero <agustin423@icloud.com>
2023-01-13 10:34:43 -0800

    Code used for 2023-01-13-07-41-22 dataset

--- acts-odd-optuna/main.py
+++ acts-odd-optuna/main.py
@@ -1,25 +1,19 @@
 import argparse
-import optuna
 import pathlib
 import optimize
 import analyze_root
 import analyze_score
+import analyze_trial
 
-def get_best_trial_params():
-    study_file_path = 'sqlite:///2023-01-10-14-09-59/study.db'
-    study_name = 'study'
-    study = optuna.load_study(storage = study_file_path,
-                              study_name = study_name)
-    print(study.best_trial)
-    
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('command')
-    parser.add_argument('paramA')
+    parser.add_argument('paramA', nargs = '?')
     args = parser.parse_args()
 
     if args.command == 'get_best_trial':
-        get_best_trial_params()
+        # args.paramA: String of the relative path to the simulation directory, e.g. 2023-01-13-00-07-47
+        analyze_trial.get_best_trial_params(args.paramA)
 
     if args.command == 'analyze_root':
         # args.paramA: String of the relative path to the simulation directory, e.g. 2023-01-13-00-07-47
