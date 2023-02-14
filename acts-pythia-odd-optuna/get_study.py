151215c018bcf2c7d7f90318f660fab01122b514
Agustin Romero <ar1@stanford.edu>
2023-02-14 12:29:41 -0800

    Rename

--- acts-pythia-odd-optuna/get_study.py
+++ acts-pythia-odd-optuna/get_study.py
@@ -1,9 +1,14 @@
 import optuna
 
-def get_best_trial(filestring):
-    # filestring of the .db file
-    study_file_path = 'sqlite:///{}'.format(filestring)
+def get_study(path):
+    study_file_path = 'sqlite:///{}'.format(path)
     study_name = 'study'
     study = optuna.load_study(storage = study_file_path,
                               study_name = study_name)
+
+    return study
+
+def get_best_trial(path):
+    # path of the .db file
+    study = get_study(path)
     print(study.best_trial)
